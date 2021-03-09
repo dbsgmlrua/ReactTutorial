ead66d91169e8d669c788f8fb5d1d8f5d5b28c5a
윤희겸 <dbsgmlrua2@gmail.com>
2021-03-09 17:44:23 +0900

    22강까지

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,17 +1,16 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+import useFetch from './userFetch';
+
 const Home = () => {
 
-    const hancleClick = (e)=>{
-        console.log('hello, ninjas', e);
-    }
-    const handleClickAgain = (name, e) =>{
-        console.log('hello' + name, e.target);
-    }
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={hancleClick}>Click me</button>
-            <button onClick={(e)=>handleClickAgain('mario', e)}>Click me again</button>
+            { error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
      );
 }
